eaf5fe4b1d647a23f69891f820567b943b248709
suraj <suraj.mane@sciverse.co.in>
2026-04-19 19:48:50 +0530

    added all three tier apps with ymal manifest

---
+++ frontend/src/App.jsx
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTitle, setNewTitle] = useState("");
+  // ---when backnd app run container
+  const API_URL = `/api/tasks`;
+
+  // ----when backedn app run locally
+  // const API_URL = `http://localhost:8080/api/tasks`;
+
+  // --- READ ---
+  const fetchTasks = async () => {
+    try {
+      const res = await fetch(API_URL);
+      const data = await res.json();
+      setTasks(data || []);
+    } catch (err) {
+      console.error("Failed to fetch:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  // --- CREATE ---
+  const addTask = async (e) => {
+    e.preventDefault();
+    await fetch(API_URL, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: newTitle, status: "Pending" }),
+    });
+    setNewTitle("");
+    fetchTasks();
+  };
+
+  // --- DELETE ---
+  const deleteTask = async (title) => {
+    await fetch(`${API_URL}/${title}`, { method: "DELETE" });
+    fetchTasks();
+  };
+
+  return (
+    <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
+      <h1>Task Tracker (Go + React) with Persistance volume</h1>
+
+      <form onSubmit={addTask} style={{ marginBottom: "20px" }}>
+        <input
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+          placeholder="Enter new task..."
+          style={{ padding: "8px", marginRight: "10px" }}
+        />
+        <button
+          type="submit"
+          style={{ padding: "8px 16px", cursor: "pointer" }}
+        >
+          Add Task
+        </button>
+      </form>
+
+      <table
+        border="1"
+        cellPadding="10"
+        style={{ width: "100%", borderCollapse: "collapse" }}
+      >
+        <thead>
+          <tr style={{ backgroundColor: "#f4f4f4" }}>
+            <th>Title</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tasks.map((task, index) => (
+            <tr key={index}>
+              <td>{task.title}</td>
+              <td>{task.status}</td>
+              <td>
+                <button
+                  onClick={() => deleteTask(task.title)}
+                  style={{
+                    color: "red",
+                    border: "1px solid red",
+                    background: "none",
+                    cursor: "pointer",
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
